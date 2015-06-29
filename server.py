382867ac11fb0eb3f54c4f0a8c2699911f0210c2
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-06-29 16:58:33 -0700

    Main Code

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
+import datetime
 
-ADDR = ('127.0.0.1', 7000)
+ADDR = ('127.0.0.1', 7076)
 
 socket = socket.socket(
     socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP
@@ -9,15 +10,32 @@ socket = socket.socket(
 socket.bind(ADDR)
 socket.listen(1)
 
+
+def response_ok():
+    content_type = "text/plain"
+    content_length = "1354"
+    date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+    Str = "HTTP/1.1 200 OK" + "\r\n" + date + "\r\n"  + "Content-Type:" + content_type + "\r\n"  + "Content-Length:" + content_length + "\r\n\r\n"
+    return Str
+
+
+def response_error():
+    return "500 Server Error \r\n\r\n"
+
+
 while True:
     try:
         conn, addr = socket.accept()
+        code = ""
+        code = response_ok()
         while True:
             msg = conn.recv(16)
             print msg
-            conn.sendall(msg)
             if len(msg) < 16:
-                conn.close()
                 break
+        conn.sendall(code)
+        conn.close()
     except KeyboardInterrupt:
         break
+
+
