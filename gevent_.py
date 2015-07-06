33317868fcd5df26910f9ce95d2d40671bb82fac
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-07-06 09:18:06 -0700

    changed filename for concurrency file

--- gevent_.py
+++ gevent_.py
@@ -106,6 +106,6 @@ if __name__ == '__main__':
     from gevent.server import StreamServer
     from gevent.monkey import patch_all
     patch_all()
-    server = StreamServer(('127.0.0.1', 10008), server_)
+    server = StreamServer(('127.0.0.1', 10007), server_)
     print('Starting server on port 10007')
     server.serve_forever()
